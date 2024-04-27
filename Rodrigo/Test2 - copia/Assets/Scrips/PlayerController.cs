9f45bf79f29f647e31e40342aa6192ce6acc7da5
rodrigoCodesmain <167713942+rodrigoCodesmain@users.noreply.github.com>
2024-04-27 17:13:58 -0400

    Codigo-rodrigo
    Test

---
+++ Rodrigo/Test2 - copia/Assets/Scrips/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Velocidad de movimiento
+    public LayerMask obstacleLayer; // Capa que representa los obstáculos
+
+    
+    void Update()
+    {
+        
+    }
+}
